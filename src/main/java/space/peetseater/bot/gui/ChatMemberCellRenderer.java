e6ac5742880dc2e82c45726d6d61a4818753f0a0
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-12 23:25:59 -0400

    Create cell renderer for chat messages
    assigns colors based on username, note that I haven't got anything
    setup yet to have chat messages remove themselves. that should probably
    be done outside the renderer since it's not its concern.

---
+++ src/main/java/space/peetseater/bot/gui/ChatMemberCellRenderer.java
@@ -0,0 +1,54 @@
+package space.peetseater.bot.gui;
+
+import space.peetseater.bot.ChatMessage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ChatMemberCellRenderer implements javax.swing.ListCellRenderer<ChatMessage> {
+
+    private final int width;
+    private int colorIdx = 0;
+    private final List<Color> chatColors;
+    private final HashMap<String, Color> chatMemberToColor;
+    private int fontSize;
+
+    public ChatMemberCellRenderer(int width, int fontSize) {
+        chatMemberToColor = new HashMap<>();
+        chatColors = new ArrayList<Color>();
+        chatColors.add(Color.WHITE);
+        chatColors.add(Color.RED);
+        chatColors.add(Color.GREEN);
+        chatColors.add(Color.CYAN);
+        chatColors.add(Color.PINK);
+        this.width = width;
+        this.fontSize = fontSize;
+    }
+
+    private Color getChatMemberColor(ChatMessage chatMessage) {
+        if (chatMemberToColor.containsKey(chatMessage.username())) {
+            return chatMemberToColor.get(chatMessage.username());
+        }
+        colorIdx++;
+        Color color = chatColors.get(colorIdx % chatColors.size());
+        System.out.println(color);
+        chatMemberToColor.put(chatMessage.username(), color);
+        return color;
+    }
+
+    // Overrideable for fun
+    protected String formatChatMessage(ChatMessage chatMessage) {
+        return "<html><body style='width: %spx'>%s: %s</body></html>".formatted(width - fontSize, chatMessage.username(), chatMessage.chatMessage());
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends ChatMessage> list, ChatMessage value, int index, boolean isSelected, boolean cellHasFocus) {
+        JLabel label = new JLabel(formatChatMessage(value));
+        label.setFont(list.getFont());
+        label.setForeground(getChatMemberColor(value));
+        return label;
+    }
+}

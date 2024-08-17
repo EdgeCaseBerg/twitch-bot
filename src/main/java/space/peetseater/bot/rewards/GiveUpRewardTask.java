c27d27ec82196663150e2c8b9d1da27891030846
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-17 10:06:38 -0400

    Add simple show then hide tasks
    Queso for the sink meme, give up for the elephant ff14 guy

--- src/main/java/space/peetseater/bot/rewards/GiveUpRewardTask.java
+++ src/main/java/space/peetseater/bot/rewards/GiveUpRewardTask.java
@@ -1,7 +1,18 @@
 package space.peetseater.bot.rewards;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class GiveUpRewardTask extends AbstractShowThenHideTask {
     public static String twitchRewardName = "I had bad day";
+
+    Logger logger = LoggerFactory.getLogger(getClass().getCanonicalName());
+
+    @Override
+    protected Logger getLogger() {
+        return logger;
+    }
+
     @Override
     protected String getSceneItemName() {
         return "give up wisdom.mp4";
--- src/main/java/space/peetseater/bot/rewards/QuesoRewardTask.java
+++ src/main/java/space/peetseater/bot/rewards/QuesoRewardTask.java
@@ -1,7 +1,17 @@
 package space.peetseater.bot.rewards;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class QuesoRewardTask extends AbstractShowThenHideTask {
     static String twitchRewardName = "Queso";
+
+    Logger logger = LoggerFactory.getLogger(getClass().getCanonicalName());
+
+    @Override
+    public Logger getLogger() {
+        return logger;
+    }
 
     @Override
     public String getSceneItemName() {
